662e7226224bebd1c6364c8f76fe8371e17ef218
jesusH <jesushuaraca23@gmail.com>
2025-11-25 22:01:45 -0500

    carga inicial

---
+++ sistemav/src/main/java/com/utp/sistemav/Sistemav.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.utp.sistemav;
+
+/**
+ *
+ * @author MK
+ */
+public class Sistemav {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
